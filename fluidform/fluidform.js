837a67393fa45d1dca433f6065b58ad4b0b33ff9
darudeSlurpstorm <davis.jack@husky.neu.edu>
2018-03-22 22:37:43 -0400

    committing to push

--- fluidform/fluidform.js
+++ fluidform/fluidform.js
@@ -20,18 +20,40 @@ window.onload = function () {
 
     // Animate section opening
     addListenerToClass("closed-section", "click", function() {
-        var intervalID = setInterval(frame, 5);
         var sectionElement = this;
-        function frame() {
+        var intervalID = setInterval(openSection, 5);
+        function openSection() {
             var computedStyle = window.getComputedStyle(sectionElement);
             var currentHeight = parseInt(computedStyle.height.slice(0, -2), 10);
-            console.log("current height: " + currentHeight);
             if (currentHeight >= 400) {
                 sectionElement.style.height = 400;
                 clearInterval(intervalID);
+                sectionElement.className = sectionElement.className.replace(/\bclosed-section\b/g, "");
+                sectionElement.className += " open-section";
             } else {
                 var increased = currentHeight + 10;
                 sectionElement.style.height = increased.toString();
+            }
+        }
+    });
+
+    // Animate section closing
+    addListenerToClass("open-section", "click", function() {
+        var sectionElement = this;
+        var intervalID = setInterval(closeSection, 5);
+        function closeSection() {
+            var computedStyle = window.getComputedStyle(sectionElement);
+            var currentHeight = parseInt(computedStyle.height.slice(0, -2), 10);
+            if (currentHeight <= 120) {
+                console.log("all done closing");
+                sectionElement.style.height = 120;
+                clearInterval(intervalID);
+                sectionElement.className = sectionElement.className.replace(/\bopen-section\b/g, "");
+                sectionElement.className += " closed-section";
+            } else {
+                console.log("trying to close...");
+                var decreased = currentHeight - 10;
+                sectionElement.style.height = decreased.toString();
             }
         }
     });
