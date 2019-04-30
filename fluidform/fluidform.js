a4a3bebacf8eb14ff7b9568ef285701f1a0444c5
jdavis <davis.jack@husky.neu.edu>
2019-04-30 11:13:32 -0500

    Learning some more about click handlers, restarting project

--- fluidform/fluidform.js
+++ fluidform/fluidform.js
@@ -1,70 +1,97 @@
-function addListenerToClass(name, event, listener) {
-    var inputs = Array.from(document.getElementsByClassName(name));
-    inputs.map(element => element.addEventListener(event, listener));
-}
+// Fluidform, written by Jack Davis
+'use strict';
 
-function performOnClass(name, action) {
-    var inputs = Array.from(document.getElementsByClassName(name));
-    inputs.map(action);
-}
-
-window.onload = function () {
-
-    // Hover style changes for closed sections
-    addListenerToClass("closed-section", "mouseover", function() {
-        this.style.backgroundColor = "#eeeeee";
-    });
-    addListenerToClass("closed-section", "mouseout", function() {
-        this.style.backgroundColor = "White";
-    });
-
-    // Animate section opening
-    addListenerToClass("closed-section", "click", function() {
-        var sectionElement = this;
-        var intervalID = setInterval(openSection, 5);
-        function openSection() {
-            var computedStyle = window.getComputedStyle(sectionElement);
-            var currentHeight = parseInt(computedStyle.height.slice(0, -2), 10);
-            if (currentHeight >= 400) {
-                sectionElement.style.height = 400;
-                clearInterval(intervalID);
-                sectionElement.className = sectionElement.className.replace(/\bclosed-section\b/g, "");
-                sectionElement.className += " open-section";
-            } else {
-                var increased = currentHeight + 10;
-                sectionElement.style.height = increased.toString();
-            }
+function sectionClickHandler() {
+    var children = this.getElementsByClassName("section-item");
+    if (this.className.includes("closed-section")) {
+        for(var i = 0; i < children.length; i++) {
+            children[i].classList.remove("hidden-item");
         }
-    });
-
-    // Animate section closing
-    addListenerToClass("open-section", "click", function() {
-        var sectionElement = this;
-        var intervalID = setInterval(closeSection, 5);
-        function closeSection() {
-            var computedStyle = window.getComputedStyle(sectionElement);
-            var currentHeight = parseInt(computedStyle.height.slice(0, -2), 10);
-            if (currentHeight <= 120) {
-                console.log("all done closing");
-                sectionElement.style.height = 120;
-                clearInterval(intervalID);
-                sectionElement.className = sectionElement.className.replace(/\bopen-section\b/g, "");
-                sectionElement.className += " closed-section";
-            } else {
-                console.log("trying to close...");
-                var decreased = currentHeight - 10;
-                sectionElement.style.height = decreased.toString();
-            }
+        this.classList.remove("closed-section");
+        this.classList.add("open-section");
+    } else {
+        for(var i = 0; i < children.length; i++) {
+            children[i].classList.add("hidden-item");
         }
-    });
+        this.classList.remove("open-section");
+        this.classList.add("closed-section");
+    }
+}
+
+function itemResponseClickHandler(event) {
+    if (event && event.stopPropagation) {
+        event.stopPropagation();
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    var sections = document.getElementsByClassName("section");
+    for(var i = 0; i < sections.length; i++) {
+        sections[i].addEventListener("click", sectionClickHandler);
+    }
+
+    var itemResponses = document.getElementsByClassName("item-response");
+    for(var i = 0; i < itemResponses.length; i++) {
+        itemResponses[i].addEventListener("click", itemResponseClickHandler);
+    }
+});
+
+const animTypes = {
+    GROW: "grow",
+    SHRINK: "shrink"
+};
+
+// AnimationQueueElement represents an animation that needs to happen
+// - id       : The id of the HTML element to be animated
+// - animType : An enum of animations that can be performed
+// - info     : An object containing information specific to the animation being performed
+//      - GROW  : originalSize (Number), targetSize (Number)
+//      - SHRINK: originalSize (Number), targetSize (Number)
+class AnimationQueueElement {
+
+    constructor(id, animType, info) {
+        this.id = id;
+        this.animType = animType;
+        this.info = info;
+    }
+
+    get id() {
+        return this.id;
+    }
+
+    get animType() {
+        return this.animType;
+    }
+
+    get target() {
+        return this.info;
+    }
 
 }
 
+// Queue for animations to be performed
+var animationQueue = [];
 
+function grow(id) {
+    animationQueue.push(new AnimationQueueElement(
+        id,
+        animTypes.GROW,
+        getGrowthInfo(id)
+    ));
+}
 
-
-
-
-
-
-
+// Calculate the information needed to successfully perform a growth animation
+function getGrowthInfo(id) {
+    var element = document.getElementById(id);
+    if (!element) {
+        throw new Error("Could not find element with id: " + id);
+    }
+    if (!element.className.match(/(?<!-)section/g)) {
+        throw new Error("This element is not a section element.");
+    }
+    var info = {};
+    var originalSize = element.offsetHeight;
+    var sectionItems = element.getElementsByClassName("section-item");
+    // TODO find a way to calculate the heights of the hidden section items
+    return info;
+}
