50899e4a6415f14f10835497673011ace88a391b
jdavis <davis.jack@husky.neu.edu>
2019-05-08 13:29:19 -0500

    Dragging And Dropping!
    Dragging and dropping now work; I made a lot of changes to the dragging part that worked before, though. The way I was doing things (catching events in the tiles themselves), I wasn't able to easily to get dropping to work without adding global state (ugh). So I reworked the handlers to fire in the tile container instead; that way, everything I needed would bubble up and I could handle it based on the event target.

--- drag-n-drop/dragndrop.js
+++ drag-n-drop/dragndrop.js
@@ -1,43 +1,122 @@
 window.addEventListener("load", (e) => {
-    let tiles = document.getElementsByClassName("tile");
 
-    Array.prototype.forEach.call(tiles, (tile) => {
-        if (!tile.dragging) {
-            tile.dragging = false;
-            tile.oldSlot = tile.parentNode;
-            tile.slotWidth = tile.offsetWidth;
-            tile.slotHeight = tile.offsetHeight;
-            tile.windowDragHandler = windowDragHandler.bind(tile/*, tile*/);
-            tile.addEventListener("mousedown", drag);
-            tile.addEventListener("mouseup", stopDragging);
+    Array.prototype.forEach.call(
+        document.getElementsByClassName("tile-container"),
+        (container) => {
+            let selectedTile = null;
+            let homeSlot = null;
+            let selectedSlot = null;
+            container.addEventListener("mousedown", (mouseDownEvent) => {
+                if (mouseDownEvent.button !== 0) {
+                    return;
+                }
+                switch(mouseDownEvent.target.className) {
+                    case "tile":
+                        console.log("Selected a tile.");
+                        selectedTile = mouseDownEvent.target;
+                        homeSlot = mouseDownEvent.target.parentNode;
+                        let oldWidth = mouseDownEvent.target.offsetWidth;
+                        let oldHeight = mouseDownEvent.target.offsetHeight;
+                        container.appendChild(homeSlot.removeChild(selectedTile));
+                        selectedTile.style.position = "absolute";
+                        selectedTile.style.width = oldWidth;
+                        selectedTile.style.height = oldHeight;
+                        selectedTile.style.left = mouseDownEvent.clientX + 10;
+                        selectedTile.style.top = mouseDownEvent.clientY + 10;
+                        break;
+
+                    case "tile-slot":
+                        console.log("Clicked on tile slot.");
+                        break;
+
+                    default:
+                        console.log("Clicked on the container.");
+                        break;
+                }
+            });
+            container.addEventListener("mouseup", (mouseUpEvent) => {
+                if (mouseUpEvent.button !== 0) {
+                    return;
+                }
+                switch(mouseUpEvent.target.className) {
+                    case "tile":
+                        break;
+
+                    case "tile-slot":
+                        if (selectedTile) {
+                            console.log("Dropping selected tile in new slot.");
+                            selectedTile.style.position = "";
+                            selectedTile.style.width = "";
+                            selectedTile.style.height = "";
+                            selectedTile.style.left = "";
+                            selectedTile.style.top = "";
+                            if (mouseUpEvent.target === homeSlot) {
+                                homeSlot.appendChild(container.removeChild(selectedTile));
+                            } else {
+                                selectedSlot.appendChild(container.removeChild(selectedTile));
+                            }
+                            selectedTile = null;
+                            selectedSlot = null;
+                            homeSlot = null;
+                        }
+                        break;
+
+                    default:
+                        if (selectedTile) {
+                            console.log("Dropped selected tile.");
+                            selectedTile.style.position = "";
+                            selectedTile.style.width = "";
+                            selectedTile.style.height = "";
+                            selectedTile.style.left = "";
+                            selectedTile.style.top = "";
+                            homeSlot.appendChild(container.removeChild(selectedTile));
+                            selectedTile = null;
+                            selectedSlot = null;
+                            homeSlot = null;
+                        }
+                        break;
+                }
+            });
+            container.addEventListener("mouseover", (mouseOverEvent) => {
+                if (mouseOverEvent.button !== 0) {
+                    return;
+                }
+                switch(mouseOverEvent.target.className) {
+                    case "tile-slot":
+                        if (selectedTile && mouseOverEvent.target !== homeSlot) {
+                            console.log("Selected a slot.");
+                            selectedSlot = mouseOverEvent.target;
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            });
+            container.addEventListener("mouseexit", (mouseExitEvent) => {
+                if (mouseExitEvent.button !== 0) {
+                    return;
+                }
+                switch(mouseExitEvent.target.className) {
+                    case "tile-slot":
+                        selectedSlot = null;
+                        break;
+
+                    default:
+                        break;
+                }
+            });
+            container.addEventListener("mousemove", (mouseMoveEvent) => {
+                if (mouseMoveEvent.button !== 0) {
+                    return;
+                }
+                if (selectedTile) {
+                    console.log("Attempting to drag.");
+                    selectedTile.style.left = mouseMoveEvent.clientX + 10;
+                    selectedTile.style.top = mouseMoveEvent.clientY + 10;
+                }
+            });
         }
-    });
+    );
 
 });
-
-function drag(dragEvent) {
-    dragEvent.stopPropagation();
-    this.dragging = true;
-    this.oldSlot.parentNode.appendChild(this.oldSlot.removeChild(this));
-    this.style.width = this.slotWidth;
-    this.style.height = this.slotHeight;
-    this.style.position = "absolute";
-    window.addEventListener("mousemove", this.windowDragHandler);
-}
-
-function windowDragHandler(/*tile, */mouseMoveEvent) {
-    followMouse(this, mouseMoveEvent.clientX, mouseMoveEvent.clientY);
-}
-
-function followMouse(tile, clientX, clientY) {
-    if (tile.dragging) {
-        tile.style.left = clientX - (tile.offsetWidth / 2);
-        tile.style.top = clientY - (tile.offsetHeight / 2);
-        setTimeout(() => followMouse(tile), 0);
-    }
-}
-
-function stopDragging(e) {
-    this.dragging = false;
-    window.removeEventListener("mousemove", windowDragHandler);
-}
